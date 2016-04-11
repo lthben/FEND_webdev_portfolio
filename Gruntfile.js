0f80ae1ace09ee175fc78711056d36c507d26e35
Benjamin Low <lthben@gmail.com>
2016-04-11 18:08:47 +0800

    adjusted image sizes

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,69 +7,19 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
-        concat: {
-            // 2. Configuration for concatinating files goes here.
-        }
 
-        uglify: {
-          build: {
-            src: 'js/build/production.js',
-            dest: 'js/build/production.min.js'
-          }
-        }
-
-        imagemin: {
-           dynamic: {
-            files: [{
-              expand: true,
-              cwd: 'images/',
-              src: ['**/*.{png,jpg,gif}'],
-              dest: 'images/build/'
-            }]
-          }
-        }
-
-        watch: {
-          scripts: {
-            files: ['js/*.js'],
-            tasks: ['concat', 'uglify'],
-            options: {
-            spawn: false,
-            },
-          }
-
-          css: {
-            files: ['css/*.scss'],
-            tasks: ['sass'],
-            options: {
-              spawn: false,
-            }
-          }
-        }
-
-
-        sass: {
-          dist: {
-            options: {
-              style: 'compressed'
-            },
-            files: {
-            'css/build/global.css': 'css/global.scss'
-            }
-          }
-        }
 
         responsive_images: {
           dev: {
             options: {
               engine: 'im',
               sizes: [{
-                name: 'small',
+                name: '',
                 width: '30%',
                 suffix: '_small',
                 quality: 20
               },{
-                name: 'large',
+                name: '',
                 width: '50%',
                 suffix: '_large',
                 quality: 40
@@ -87,9 +37,11 @@ module.exports = function(grunt) {
     });
 
     // 3. Where we tell Grunt we plan to use this plug-in.
-    grunt.loadNpmTasks('grunt-contrib-concat', 'grunt-contrib-uglify', 'grunt-contrib-imagemin', 'grunt-contrib-watch', 'grunt-contrib-sass', 'grunt-responsive-images');
+    //grunt.loadNpmTasks('grunt-contrib-concat', 'grunt-contrib-uglify', 'grunt-contrib-imagemin', 'grunt-contrib-watch', 'grunt-contrib-sass', 'grunt-responsive-images');
+    grunt.loadNpmTasks('grunt-responsive-images');
+
 
     // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-    grunt.registerTask('default', ['concat', 'uglify', 'imagemin', 'watch', 'sass']);
+    grunt.registerTask('default', ['responsive_images']);
 
 };
